522ac6ff055272bbe7b238cab2e70b9ed1867180
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-02-01 03:34:08 +0600

    Rotate Arrays.cpp

--- Rotate Arrays.cpp
+++ Rotate Arrays.cpp
@@ -8,4 +8,17 @@ int main() {
   //rotate by k places 
   int k;
   cin>>k;
+  //to avoid missing the later values you need to have a separate array 
+  vector<int>v(n);
+  //(k+i)%n = i 
+  for(auto i=0;i<5;i++){
+   v[(k+i)%n]=arr[i];
+  }
+  for(auto i=0;i<v.size();i++){
+    cout<<v.at(i)<<" ";
+  }
   
+
+
+  return 0;
+}
