4b971c0d2dcab763efad0d84c8238ffad499acec
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-09-02 23:30:06 +0600

    Including the Main

--- OOP/Cookies-Class.cpp
+++ OOP/Cookies-Class.cpp
@@ -13,9 +13,22 @@ public:
         cout<<"\n"<<this->color;
         return this->color;
     }
-  void setColor(string newColor){
+   void setColor(string newColor){
         this->color=newColor;
         cout<<"Color Set to: "<<newColor;
     }
 
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    Cookie* myCookie1=new Cookie("red");
+    Cookie* myCookie2=new Cookie("Orange");
+    myCookie2->getColor();
+    myCookie2->setColor("navy");
+    myCookie2->getColor();
+
+
+
+    return 0;
 }
