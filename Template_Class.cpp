05af4e159cc9d04dca8fbe7de85dcb7d7ca0ebad
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-09-14 22:02:05 +0600

    Update Template_Class.cpp

--- Template_Class.cpp
+++ Template_Class.cpp
@@ -6,3 +6,13 @@ class makeDynamicArrays{
 private:
     int size;
     T* arr;
+public:
+    makeDynamicArrays(int n){
+        this->size=n;
+        arr=new T[n];
+    }
+    void iterate(){
+        for(int i=0;i<size;i++){
+            cin>>arr[i];
+        }
+    }
