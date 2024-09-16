4a33e169f566721814cafd11b2f1c016f3dfad64
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-09-17 02:58:20 +0600

    Update ComplexNumber.cpp

--- OOP/Complex-Numbers/ComplexNumber.cpp
+++ OOP/Complex-Numbers/ComplexNumber.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+class ComplexNumber{
+    double real;
+    double imaginary;
+public:
+    friend ComplexNumber Add(ComplexNumber,ComplexNumber);
+    ComplexNumber(){
+        real=0;
+        imaginary=0;
+    }
