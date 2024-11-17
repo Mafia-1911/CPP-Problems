0e46b6911f496c96503c716464e81cbe5467b10c
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-11-17 21:45:37 +0600

    Update twoStack.cpp

--- Data Structures/twoStack.cpp
+++ Data Structures/twoStack.cpp
@@ -7,3 +7,17 @@ public:
     int top1;
     int top2;
     int* stackArr;
+
+    twoStack(int size){
+        stackArr=new int[size];
+        this->top1=-1;
+        this->top2=-1;
+    }
+    void push1(int value){
+        if(top2-top1>1){
+            top1++;
+            stackArr[top1]=value;
+        }else{
+            cout<<"Stack Overflow"<<endl;
+        }
+    }
