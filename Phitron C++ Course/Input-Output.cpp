1d73fd5d7a55de6e4b9720e2be0fe407c916e589
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2024-12-30 23:09:11 +0600

    Create Input-Output.cpp

---
+++ Phitron C++ Course/Input-Output.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    //When we dont the number of inputs use
+    int tc;
+    while(cin>>tc){
+        //do the task
+    }
+
+    double output=2.2223221;
+    //set Precision - just enter the precision amount
+    cout<< fixed << setprecision(2) << output << endl;
+
+    int x=120;
+    x%2==0 ? cout<<"Its Even" : cout<<"Its Odd";
+
+}
