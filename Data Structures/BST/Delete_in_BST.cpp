e262acda092ec98409acd13315c50d644e8222a7
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2025-01-30 21:58:44 +0600

    Update Delete_in_BST.cpp

--- Data Structures/BST/Delete_in_BST.cpp
+++ Data Structures/BST/Delete_in_BST.cpp
@@ -18,3 +18,48 @@ public:
 // For 2 nodes : take the min value from the right subtree
 //              or Take the maximum value from the left subtree
 // we shall boil the 2nodes case into deleting a 0 child node , Thats why we find the leftmost or rightmost
+
+
+Node* deleteFromBST(Node* root,int target){
+    if(root==nullptr){
+
+    }
+    if(root->data == target){
+        //no child
+        if(root->left == nullptr && root->right== nullptr){
+            Node* temp = root;
+            delete root;
+            return temp;
+        }
+
+        //1 child
+            //Left Child
+            if(root->left != nullptr && root->right == nullptr){
+                Node* temp=root;
+                delete root;
+                return temp;
+            }
+            //Right Child
+            if(root->right != nullptr && root->left == nullptr){
+            Node* temp=root;
+            delete root;
+            return temp;
+        }
+
+        //2 child
+            if(root->left != nullptr && root->right != nullptr){
+                //Finding value of the leftmost in the right subtree
+                int minimum=findMin(root->right) -> data;
+                //swapping the value
+                root->data = minimum;
+                //deleting that 0 child node
+                root->right= deleteFromBST(root->right,minimum);
+                return root;
+            }
+    }
+    else if(target > root->data){
+        deleteFromBST(root->right,target);
+    }else if(target < root->data){
+        deleteFromBST(root->left,target);
+    }
+}
