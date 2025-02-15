8a6ae47953c43211692d694f5ea5c1f8e8c01d45
Mushtasin Ahmed <74264555+Mafia-1911@users.noreply.github.com>
2025-02-15 15:19:48 +0600

    Update Array__of__Objects.cpp

--- Phitron C++ Course/Array__of__Objects.cpp
+++ Phitron C++ Course/Array__of__Objects.cpp
@@ -1,1 +1,40 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
 
+
+class Student{
+public:
+    string name;
+    int id;
+    int marks;
+};
+/////Custom Compare Function
+//Return the condition you want thats it !
+//OR use if else -> true on the order you want , Since it works with two obejcts at a time
+bool cmp(Student left,Student right){
+    //    if(left.marks<right.marks){
+    //        return true;
+    //    }else if(left.marks > right.marks){
+    //        return false;
+    //    }
+    return left.marks<right.marks;
+}
+bool cmpD(Student left,Student right){
+    if(left.marks>right.marks){
+        return true;
+    }else return false;
+}
+
+bool crazyCMP(Student left,Student right){
+    //Ascending Sort
+    if(left.marks < right.marks ){
+        return true;
+    }else if( left.marks>right.marks){
+        return false;
+    }else {
+        //If the marks are same , Sort using the roll
+        if(left.id<right.id) return true;
+        else return false ;
+    }
+}
